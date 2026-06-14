91be6b4aa41bade282d8aa7399e36e0dd06375eb
moraamasr5-create <moraamasr5@gmail.com>
2026-06-14 20:10:52 +0300

    Update Sidebar.jsx

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -157,7 +157,7 @@ const Sidebar = ({ activeTab, setActiveTab, isSidebarOpen, closeSidebar, onOpenS
                                     }
                                 }}
                                 className="btn-primary"
-                                style={{ width: '50%', background: 'var(--danger)' }}
+                                style={{ width: '75%', background: 'var(--danger)' }}
                             >
                                 <Square size={18} />
                                 <span>إغلاق الوردية</span>
